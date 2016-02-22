d3736108685968fac4a2b72a8866a6f81f6524c5
Ashish Kumar <ashish@ashish.co>
2016-02-22 22:32:34 +0100

    Add setup script to simplify configuration

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/.bowerrc ~/.bowerrc
+ln -s ~/.dotfiles/.gemrc ~/.gemrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+
