fae00a432306fca4fcbcd381bfab6d2896c93151
Ashish Kumar <ashish@ashish.co>
2017-12-05 15:53:53 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/.dotfiles/.bowerrc ~/.bowerrc
-ln -s ~/.dotfiles/.gemrc ~/.gemrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+cd ~
 
+ln -s ~/.dotfiles/.bash_profile
+ln -s ~/.dotfiles/.bowerrc
+ln -s ~/.dotfiles/.gemrc
+ln -s ~/.dotfiles/.gitconfig
+ln -s ~/.dotfiles/.gitignore_global
+ln -s ~/.dotfiles/.vim
+ln -s ~/.dotfiles/.vimrc
+
+cd -
